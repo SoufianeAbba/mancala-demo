0f7d0090267edbd747b9b37d5bcf4f0c73eeb2f9
Soufiane Abba <soufiane.abba@topicus.nl>
2022-11-28 00:21:27 +0100

    Remove empty constructor from record.

--- mancalademo/src/main/java/com/sabba/mancalademo/model/Payload.java
+++ mancalademo/src/main/java/com/sabba/mancalademo/model/Payload.java
@@ -1,8 +1,6 @@
 package com.sabba.mancalademo.model;
 
-public record Payload (GameSession gameSession, String errorMessage, PayloadActionEnum payloadAction) {
-	public Payload() {
-		this(null, "", null);
+public record Payload (GameSession gameSession, String errorMessage, PayloadActionEnum payloadAction)
+{
 
-	}
 }
